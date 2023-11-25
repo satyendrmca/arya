857d2061c534ae35b17a73bc465b2ef77ad6a5a9
satyendrmca <satyendrmca@gmail.com>
2023-11-25 13:54:30 +0530

    Update TestController.java

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/v1")
+@RestController
 public class TestController {
 
 	
